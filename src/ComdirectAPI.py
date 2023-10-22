cdbf49e82eb073110e603d73ab33c802d517906c
Jonas Paczia <jpaczia@users.noreply.github.com>
2023-10-22 18:55:04 +0200

    Ouptut unmatched and saved files

--- src/ComdirectAPI.py
+++ src/ComdirectAPI.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, Field
 import os
 from tqdm import tqdm
+import logging
+import typing
 
 from src.handler.DocumentHandler import DocumentHandler
 from src.data.Document import Document
@@ -21,16 +23,40 @@ class ComdirectAPI(BaseModel):
         """Process all documents in the postbox"""
         documents = self.document_handler.get_all_postbox_contents()
 
+        unmatched_file_names: typing.List[str] = []
+        saved_files: typing.List[str] = []
         for doc in tqdm(documents, desc="Process documents in postbox"):
-            self.process_document(doc)
+            doc_path, unmatched = self.process_document(doc)
+            if doc_path is not None:
+                saved_files.append(doc_path)
+            if unmatched:
+                unmatched_file_names.append(doc.name)
 
-    def process_document(self, document: Document) -> None:
+        if len(saved_files) > 0:
+            # print all unmatched files in case of new names or similar cases
+            print("\n\n\n")
+            print("#########################################################")
+            logging.info("Saved files:")
+            for saved_file in saved_files:
+                logging.info(saved_file)
+
+        if len(unmatched_file_names) > 0:
+            # print all unmatched files in case of new names or similar cases
+            print("\n\n\n")
+            print("#########################################################")
+            logging.info("Unmatched files:\n")
+            for saved_file in unmatched_file_names:
+                logging.info(saved_file)
+
+    def process_document(
+        self, document: Document
+    ) -> typing.Tuple[typing.Optional[str], bool]:
         """Download document from the postbox if it's new and no ad.
         Skip documents which have been downloaded already."""
         if document.skip_download(
             self.document_classification_config.ignored_file_classes
         ):
-            return
+            return None, False
 
         (
             matched_wkn,
@@ -47,10 +73,14 @@ class ComdirectAPI(BaseModel):
 
         if os.path.exists(doc_path):
             # skip files which already have been downloaded
-            return
+            return None, False
 
         _, document_content = self.document_handler.get_document(
             document_id=document.document_id, document_mime_type=document.mime_type
         )
         file_utils.save_pdf(content=document_content, pdf_path=doc_path)
-        print(f"Saved document: {doc_path}")
+
+        return doc_path, self.document_classification_config.unknown_classification in [
+            matched_wkn,
+            matched_file_class,
+        ]
